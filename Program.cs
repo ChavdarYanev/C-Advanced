67d7e4c210d4d533f19e3609b1853f95bb155822
Chavdar Yanev <45217541+ChavdarYanev@users.noreply.github.com>
2019-05-19 05:15:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string text = Console.ReadLine();
+			Stack<char> stack = new Stack<char>();
+
+			foreach (var item in text)
+			{
+				stack.Push(item);
+			}
+
+			while (stack.Count>0)
+			{
+				Console.Write(stack.Pop());
+			}
+			Console.WriteLine();
+		}
+	}
+}
